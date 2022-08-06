4e98ea29c962614ed2879304299328552de859cd
diogocastros1 <diogoantonio@gec.inatel.br>
2022-08-05 23:03:41 -0300

    Atualização do README e novos exemplos

--- Exemplos/for1.js
+++ Exemplos/for1.js
@@ -1,8 +1,8 @@
-for(let i = 1; i <= 10; i++){
+for (let i = 1; i <= 10; i++) {
     console.log(`i = ${i}`)
 }
 
 const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
-for(i = 0; i < notas.length; i++){
-    console.log(`nota ${i+1} = ${notas[i]}`)
+for (i = 0; i < notas.length; i++) {
+    console.log(`nota ${i + 1} = ${notas[i]}`)
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# web-moderno
-Curso de Web Moderno
+# Curso de Web Moderno
+
+Este repositório é destinado para os exercicios e exemplos do curso de Web Moderno.
