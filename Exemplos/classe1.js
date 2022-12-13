21f261c5abf94c6450039050133e0210eb84f1fd
diogocastros1 <diogoantonio@gec.inatel.br>
2022-12-13 20:27:12 -0300

    Classes e funcao construtora - Aula 129

--- Exemplos/classe1.js
+++ Exemplos/classe1.js
@@ -1,4 +1,5 @@
 class Lancamento {
+    //funcao construtora
     constructor(nome = 'Genérico', valor = 0) {
         this.nome = nome
         this.valor = valor
@@ -6,17 +7,19 @@ class Lancamento {
 }
 
 class CicloFinanceiro {
+    //funcao construtora
     constructor(mes, ano) {
         this.mes = mes
         this.ano = ano
         this.lancamentos = []
     }
 
-
+    //funcao esta concatenando todos os elementos dentro do array lancamento
     addLancamentos(...lancamentos) {
         lancamentos.forEach(l => this.lancamentos.push(l));
     }
 
+    //somatorio de todos os valores lancados
     sumario() {
         let valorConsolidado = 0
         this.lancamentos.forEach(l => {
@@ -27,8 +30,8 @@ class CicloFinanceiro {
 }
 
 const salario = new Lancamento('Salario', 45000)
-const contaDeLuz = new Lancamento('Luz', -200
-)
+const contaDeLuz = new Lancamento('Luz', -200)
+
 const contas = new CicloFinanceiro(12, 2022)
 contas.addLancamentos(salario, contaDeLuz)
 console.log(contas.sumario())
