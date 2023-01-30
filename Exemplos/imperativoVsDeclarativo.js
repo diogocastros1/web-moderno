aacf89a976268bf8f0906ed3fa6144584429ff2a
Diogo Castro <diogoantonio@ges.inatel.br>
2023-01-30 18:10:20 -0300

    Aula 145 | Demonstração do concat

--- Exemplos/imperativoVsDeclarativo.js
+++ Exemplos/imperativoVsDeclarativo.js
@@ -19,6 +19,7 @@ Mais preocupado em como fazer do que o que precisa. */
 const getNota = aluno => aluno.nota
 const soma = (total, atual) => total + atual
 const total2 = alunos.map(getNota).reduce(soma)
+
 console.log(total2/alunos.length)
 
 /* No metodo Declarativo temos uma forma um pouco mais complexa
