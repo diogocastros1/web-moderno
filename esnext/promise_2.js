b87ad0f8f07ea5d102af311ae7b7143b9b9c422c
Diogo Castro <diogoantonio@ges.inatel.br>
2023-02-08 17:52:12 -0300

    Aula 189 | Mais conceitos de promises

---
+++ esnext/promise_2.js
@@ -0,0 +1,30 @@
+const { functionsIn } = require("lodash")
+
+// setTimeout(function() {
+//     console.log('Executando callback...')
+
+//     setTimeout(function() {
+//         console.log('Executando callback...')
+
+//         setTimeout(function() {
+//             console.log('Executando callback...')
+
+//         }, 2000)
+//     }, 2000)
+// }, 2000)
+
+function esperarPor(tempo = 2000) {
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            console.log('Executando promise...')
+            resolve('Vishee')
+        }, tempo)
+    })
+}
+
+// esperarPor(3000)
+//     .then(texto => console.log(texto))
+
+esperarPor()
+    .then(() => esperarPor())
+    .then(esperarPor)
