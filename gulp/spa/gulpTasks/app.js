12b38cd320e85d0156145bd210eb6d39ed2ea0b5
Diogo Castro <diogoantonio@ges.inatel.br>
2023-04-18 08:51:40 -0300

    Aula 332 | Construção de Build
    Criando tasks com para um projeto SPA

--- gulp/spa/gulpTasks/app.js
+++ gulp/spa/gulpTasks/app.js
@@ -1,21 +1,39 @@
 const gulp = require('gulp')
+const babel = require('gulp-babel')
+const uglify = require('gulp-uglify')
+const sass = require('gulp-sass')(require('sass'))
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
+const htmlmin = require('gulp-htmlmin')
 
 function appHTML(cb) {
-
-    return cb()
+    return gulp.src('src/**/*.html') // selecionando todos arquivos html dentro de src e subpastas
+        .pipe(htmlmin({collapseWhitespace: true})) // removendo espaços em branco
+        .pipe(gulp.dest('build'))
 }
-function appCSS(cb) {
-
-    return cb()
+function appCSS() {
+    return gulp.src('src/assets/sass/index.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(uglifycss({"uglyComments": true}))
+        .pipe(concat('app.min.css'))
+        .pipe(gulp.dest('build/assets/css'))
 }
-function appJS(cb) {
-
-    return cb()
+function appJS() {
+    return gulp.src('src/assets/js/**/*.js') // selecionando todos arquivos JS dentro de js e subpastas
+        .pipe(babel({presets: ['ENV']}))
+        .pipe(uglify())
+        .pipe(concat('app.min.js'))
+        .pipe(gulp.dest('build/assets/js'))
 }
-function appIMG(cb) {
-
-    return cb()
+function appIMG() {
+    return gulp.src('src/assets/imgs/**/*.*') // pegando todas as imagens (PNG, JPG, JPEG entre outros) dentro de imgs e subpastas
+        .pipe(gulp.dest('build/assets/imgs'))
 }
+
+gulp.task('appHTML', appHTML)
+gulp.task('appCSS', appCSS)
+gulp.task('appJS', appJS)
+gulp.task('appIMG', appIMG)
 
 module.exports = {
     appHTML,
