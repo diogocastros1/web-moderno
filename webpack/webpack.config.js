f8cc4eb82304619ae4f9d9fe980de1f16436e14b
Diogo Castro <diogoantonio@ges.inatel.br>
2023-04-18 11:32:31 -0300

    Aula 340 | Externalizando os arquivos

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     mode: 'development', // modo de operação de desenvolvimento, não minifica os arquivos
@@ -8,12 +9,18 @@ module.exports = {
         filename: 'principal.js', // nome do arquivo de saida
         path: __dirname + '/public', // pasta do arquivo de saida
     },
-    // 
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "estilo.css"
+        })
+    ],
+    // interpretação de modulos css
     module: {
         rules: [{
             test: /\.css$/, // regex para ler css
             use: [
-                'style-loader', // adiciona CSS a DOM injetando a tag <style>
+                MiniCssExtractPlugin.loader, // este plugin substitui o uso do style-loader
+                // 'style-loader', // adiciona CSS a DOM injetando a tag <style>
                 'css-loader', // interpreta @import, url()....
             ]
         }]
